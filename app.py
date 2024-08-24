0f0ef39827d1882836e9290348f7a6974a0f976f
Daniel Sykes <sykespro@gmail.com>
2024-08-24 10:59:50 -0400

    poc vmrx without functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, jsonify)
 
 app = Flask(__name__)
 
@@ -27,6 +27,23 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+def query_llm(question): 
+    # response = llm_chain.invoke({'question': question})
+    # mock response like of object to be used like response.content
+    response = {'content': 'This is a mock response for the question: ' + question}
+    return response 
+
+@app.route('/poc/vmrx')
+def vmrx_index():
+   print('Request for vmrx page received')
+   return render_template('vmrx/index.html')
+
+@app.route("/poc/vmrx/chatbot", methods=["POST"]) 
+def vmrx_chatbot(): 
+    data = request.get_json() 
+    question = data["question"] 
+    response = query_llm(question)   
+    return jsonify({"response": response['content']})
 
 if __name__ == '__main__':
    app.run()
